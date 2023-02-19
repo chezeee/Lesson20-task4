1be8b7527df2f4ea22f4fd9de2ad2e674a9f727f
Ilya Yalymov <chezeee@gmail.com>
2023-02-19 15:26:30 +0300

    Solution added

--- src/js/index.js
+++ src/js/index.js
@@ -7,16 +7,17 @@ const prepareStatus = (status, location) => {
   const data = {
     userId,
     status,
-    location
+    location,
   };
 
-	// your code
-}
-
+  const userStatus = JSON.stringify(data);
+  console.log(userStatus);
+  return userStatus;
+};
 
 // Sample usage - do not modify
-prepareStatus("My first post!", "Amsterdam"); 
+prepareStatus("My first post!", "Amsterdam");
 // '{"userId":42,"status":"My first post!","location":"Amsterdam"}'
 
-prepareStatus("Hello World!", "Berlin"); 
+prepareStatus("Hello World!", "Berlin");
 // '{"userId":42,"status":"Hello World!","location":"Berlin"}'
